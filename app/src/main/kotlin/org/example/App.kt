8c7f483aadcc8de81d52bc7f80b4a0d3e43c518e
noah <noahlucianograna@gmail.com>
2025-05-06 09:03:25 -0300

    feat: Cálculo de valores de la función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,12 +1,18 @@
 
 package org.example
-
+fun calculo_cuadratica(a:Double, b:Double, c:Double, x:Double): Double{
+    return a*x*x+b*x+c
+}
 fun main() {
-    val a = 3
-    val b = -6
-    val c = 21
-    if (a == 0){
+    val a = 3.0
+    val b = -6.0
+    val c = 21.0
+    if (a == 0.0){
         println("a tiene que ser distinto a 0")
     }
     println("($a)x²+($b)x+($c)")
+    for (i in -5..5){
+        var x = i.toDouble()
+        println("para x = $i, y = ${calculo_cuadratica(a,b,c,x)}")
+    }
 }

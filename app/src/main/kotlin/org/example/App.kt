eb18c193c4e83b92a24914204645606e1d7a7bd4
Noah <noahlucianograna@gmail.com>
2025-05-10 00:02:38 -0300

    feat: Cálculo de raíces de la función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,12 +1,25 @@
-
+﻿
 package org.example
+import kotlin.math.pow
+import java.text.DecimalFormat
 fun calculo_cuadratica(a:Double, b:Double, c:Double, x:Double): Double{
     return a*x*x+b*x+c
 }
+fun discriminante(a:Double, b:Double, c:Double): Double{
+    return b*b-4*a*c
+}
+fun raices(a:Double, b:Double, c:Double): String{
+    val formato = DecimalFormat("0.00")
+    var raiz = discriminante(a,b,c).pow(1.0/2.0)
+    if(discriminante(a,b,c)<0){
+        println("discriminante negativo, no se pueden calcular raices")
+    }
+    return ("x1 = ${formato.format((-b+ raiz)/(2.0*a))} \nx2 = ${formato.format((-b- raiz)/(2.0*a))}")
+}
 fun main() {
     val a = 3.0
-    val b = -6.0
-    val c = 21.0
+    val b = -8.0
+    val c = 4.0
     if (a == 0.0){
         println("a tiene que ser distinto a 0")
     }
@@ -15,4 +28,6 @@ fun main() {
         var x = i.toDouble()
         println("para x = $i, y = ${calculo_cuadratica(a,b,c,x)}")
     }
+    println(discriminante(a, b, c))
+    println(raices(a, b, c))
 }
